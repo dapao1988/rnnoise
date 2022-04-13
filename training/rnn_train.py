d804669d89eeea3f2a83b10276d7e1e09c00a851
Wenbing.Wang <wangwenbingood1988@gmail.com>
2022-04-13 15:46:26 +0800

    add ModelCheckpoint for period store model weight

--- training/rnn_train.py
+++ training/rnn_train.py
@@ -2,8 +2,11 @@
 
 from __future__ import print_function
 
+import os
+import sys
 import tensorflow as tf
 import tensorflow.keras as K
+import matplotlib.pyplot as plt
 #from tensorflow import keras as K
 
 #from keras.models import Sequential
@@ -19,6 +22,7 @@ from tensorflow.keras.layers import concatenate
 from tensorflow.keras import losses
 from tensorflow.keras import regularizers
 from tensorflow.keras.constraints import min_max_norm
+from tensorflow.keras.callbacks import ModelCheckpoint
 import h5py
 
 from tensorflow.keras.constraints import Constraint
@@ -60,61 +64,71 @@ class WeightClip(Constraint):
         return {'name': self.__class__.__name__,
             'c': self.c}
 
-reg = 0.000001
-constraint = WeightClip(0.499)
+if '__main__' == __name__:
+    if len(sys.argv) < 2:
+        save_dir = '.' # 当前目录
+    else:
+        save_dir = sys.argv[1]
+        print("model save path:{}".format(os.path.abspath('.') ))
 
-print('Build model...')
-main_input = Input(shape=(None, 42), name='main_input')
-tmp = Dense(24, activation='tanh', name='input_dense', kernel_constraint=constraint, bias_constraint=constraint)(main_input)
-vad_gru = GRU(24, activation='tanh', recurrent_activation='sigmoid', return_sequences=True, name='vad_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint, reset_after=False)(tmp)
-vad_output = Dense(1, activation='sigmoid', name='vad_output', kernel_constraint=constraint, bias_constraint=constraint)(vad_gru)
-noise_input = tf.keras.layers.concatenate([tmp, vad_gru, main_input])
-noise_gru = GRU(48, activation='relu', recurrent_activation='sigmoid', return_sequences=True, name='noise_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint, reset_after=False)(noise_input)
-denoise_input = tf.keras.layers.concatenate([vad_gru, noise_gru, main_input])
+    reg = 0.000001
+    constraint = WeightClip(0.499)
 
-denoise_gru = GRU(96, activation='tanh', recurrent_activation='sigmoid', return_sequences=True, name='denoise_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint, reset_after=False)(denoise_input)
+    print('Build model...')
+    main_input = Input(shape=(None, 42), name='main_input')
+    tmp = Dense(24, activation='tanh', name='input_dense', kernel_constraint=constraint, bias_constraint=constraint)(main_input)
+    vad_gru = GRU(24, activation='tanh', recurrent_activation='sigmoid', return_sequences=True, name='vad_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint, reset_after=False)(tmp)
+    vad_output = Dense(1, activation='sigmoid', name='vad_output', kernel_constraint=constraint, bias_constraint=constraint)(vad_gru)
+    noise_input = tf.keras.layers.concatenate([tmp, vad_gru, main_input])
+    noise_gru = GRU(48, activation='relu', recurrent_activation='sigmoid', return_sequences=True, name='noise_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint, reset_after=False)(noise_input)
+    denoise_input = tf.keras.layers.concatenate([vad_gru, noise_gru, main_input])
 
-denoise_output = Dense(22, activation='sigmoid', name='denoise_output', kernel_constraint=constraint, bias_constraint=constraint)(denoise_gru)
+    denoise_gru = GRU(96, activation='tanh', recurrent_activation='sigmoid', return_sequences=True, name='denoise_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint, reset_after=False)(denoise_input)
 
-model = Model(inputs=main_input, outputs=[denoise_output, vad_output])
+    denoise_output = Dense(22, activation='sigmoid', name='denoise_output', kernel_constraint=constraint, bias_constraint=constraint)(denoise_gru)
 
-model.compile(loss=[mycost, my_crossentropy],
-              metrics=[msse],
-              optimizer='adam', loss_weights=[10, 0.5])
+    model = Model(inputs=main_input, outputs=[denoise_output, vad_output])
 
+    model.compile(loss=[mycost, my_crossentropy],
+                  metrics=[msse],
+                  optimizer='adam', loss_weights=[10, 0.5])
 
-batch_size = 32
+    checkpointer = ModelCheckpoint(os.path.join(save_dir, 'model_weights.{epoch:02d}-{val_loss:.2f}.hdf5'),
+                                               verbose=1, save_weights_only=False, period=3)
 
-print('Loading data...')
-with h5py.File('training.h5', 'r') as hf:
-    all_data = hf['data'][:]
-print('done.')
+    batch_size = 128 #32
 
-window_size = 2000
+    print('Loading data...')
+    with h5py.File('training.h5', 'r') as hf:
+        all_data = hf['data'][:]
+    print('done.')
 
-nb_sequences = len(all_data)//window_size
-print(nb_sequences, ' sequences')
-x_train = all_data[:nb_sequences*window_size, :42]
-x_train = np.reshape(x_train, (nb_sequences, window_size, 42))
+    window_size = 2000
 
-y_train = np.copy(all_data[:nb_sequences*window_size, 42:64])
-y_train = np.reshape(y_train, (nb_sequences, window_size, 22))
+    nb_sequences = len(all_data)//window_size
+    print(nb_sequences, ' sequences')
+    x_train = all_data[:nb_sequences*window_size, :42]
+    x_train = np.reshape(x_train, (nb_sequences, window_size, 42))
 
-noise_train = np.copy(all_data[:nb_sequences*window_size, 64:86])
-noise_train = np.reshape(noise_train, (nb_sequences, window_size, 22))
+    y_train = np.copy(all_data[:nb_sequences*window_size, 42:64])
+    y_train = np.reshape(y_train, (nb_sequences, window_size, 22))
 
-vad_train = np.copy(all_data[:nb_sequences*window_size, 86:87])
-vad_train = np.reshape(vad_train, (nb_sequences, window_size, 1))
+    noise_train = np.copy(all_data[:nb_sequences*window_size, 64:86])
+    noise_train = np.reshape(noise_train, (nb_sequences, window_size, 22))
 
-all_data = 0;
-#x_train = x_train.astype('float32')
-#y_train = y_train.astype('float32')
+    vad_train = np.copy(all_data[:nb_sequences*window_size, 86:87])
+    vad_train = np.reshape(vad_train, (nb_sequences, window_size, 1))
 
-print(len(x_train), 'train sequences. x shape =', x_train.shape, 'y shape = ', y_train.shape)
+    all_data = 0;
+    #x_train = x_train.astype('float32')
+    #y_train = y_train.astype('float32')
 
-print('Train...')
-model.fit(x_train, [y_train, vad_train],
-          batch_size=batch_size,
-          epochs=120,
-          validation_split=0.1)
-model.save("weights.hdf5")
+    print(len(x_train), 'train sequences. x shape =', x_train.shape, 'y shape = ', y_train.shape)
+
+    print('Train...')
+    model.fit(x_train, [y_train, vad_train],
+              batch_size=batch_size,
+              epochs=120,
+              validation_split=0.1,
+              callbacks=[checkpointer])
+    model.save("weights.hdf5")
